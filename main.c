067f1b9065b3f3bf054b7509445256c4167ecb48
Kiberbiborg <bubeev.maks@bk.ru>
2023-06-12 20:52:51 +0300

    git remote add origin https://github.com/Kiberbiborg/vector2.git git branch -M main git push -u origin main

--- main.c
+++ main.c
@@ -1,15 +1,5 @@
 #include "main.h"
 
-void test(){
-    test_pushBack_emptyVector();
-    test_pushBack_fullVector();
-    test_popBack_notEmptyVector();
-    test_atVector_notEmptyVector();
-    test_atVector_requestToLastElement();
-    test_back_oneElementInVector();
-    test_front_oneElementInVector();
-}
-
 int main() {
     vector v = createVector(5);
     v.data[0] = 1;
@@ -17,34 +7,6 @@ int main() {
     v.data[2] = 3;
     v.data[3] = 4;
     v.data[4] = 5;
-
-    printVector(&v);
-
-    reserve(&v, 3);
-
-    printVector1(&v);
-
-    shrinkToFit(&v);
-
-    printVector1(&v);
-
-    printf("%d\n", getVectorValue(&v, 3));
-
-    int *a = atVector(&v, 1);
-
-    printf("%d\n", *a);
-
-    int *back1 = back(&v);
-
-    printf("%d\n", *back1);
-
-    int *front1 = front(&v);
-
-    printf("%d\n", *front1);
-
-    deleteVector(&v);
-
-    printVector(&v);
 
     return 0;
 }
